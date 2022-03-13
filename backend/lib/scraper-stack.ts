696a25199b19135ecde9e932f2a011fc5950ce75
AmmarRahman <ammar.rahman@gmail.com>
2022-03-13 14:21:19 +0000

    Data collection

--- backend/lib/scraper-stack.ts
+++ backend/lib/scraper-stack.ts
@@ -1,10 +1,11 @@
-import { aws_dynamodb as dynamodb, aws_lambda_nodejs, Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { aws_dynamodb as dynamodb, aws_events, aws_events_targets, aws_lambda_nodejs, Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 
 export class ScraperStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
     //DynamoDb
     const table = new dynamodb.Table(this, 'NewsTable', {
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
@@ -15,31 +16,25 @@ export class ScraperStack extends Stack {
 
 
     //Lambda
-      const scraper = new aws_lambda_nodejs.NodejsFunction(this, 'Scraper', {
-        entry: './src/scraper.ts',
-  
-        timeout: Duration.seconds(300),
-        environment: {
-          TABLE_NAME: table.tableName,
-          NEWS_URL: 'https://aws.amazon.com/api/dirs/items/search?item.directoryId=whats-new&sort_by=item.additionalFields.postDateTime&sort_order=desc&size=25&item.locale=en_US&page=0'
-        },
-       
-      });
-    // const newLambda = new lambda.Function(this, 'newLambda',{
-    //   runtime: lambda.Runtime.PYTHON_3_8,
-    //   code: lambda.Code.fromAsset('functions'),
-    //   handler: 'index.handler',
-    // });
-    // const lambdaTarget = new targets.LambdaFunction(newLambda)
-    // const eventRule = new events.Rule(this, 'scheduleRule', {
-    //   schedule: events.Schedule.cron({ minute: '0', hour: '1' }),
-    //   targets: [lambdaTarget]
-    // });
+    const scraper = new aws_lambda_nodejs.NodejsFunction(this, 'Scraper', {
+      entry: './src/scraper.ts',
+      timeout: Duration.seconds(300),
+      environment: {
+        TABLE_NAME: table.tableName,
+        SEARCH_URL: 'https://aws.amazon.com/api/dirs/items/search'
+      },
+
+    });
+
+    //Role
+    table.grantReadWriteData(scraper.grantPrincipal)
 
 
+    //Scheudler
+    const eventRule = new aws_events.Rule(this, 'ScrapingSchedule', {
+      schedule: aws_events.Schedule.rate(Duration.hours(6)),
+    });
+    eventRule.addTarget(new aws_events_targets.LambdaFunction(scraper))
 
-    //table.grantReadData(lambda.grantPrincipal)
-
-    //Target
   }
 }
--- backend/src/scraper.ts
+++ backend/src/scraper.ts
@@ -1,33 +1,74 @@
+//issues
+// https://stackoverflow.com/questions/66275648/aws-javascript-sdk-v3-typescript-doesnt-compile-due-to-error-ts2304-cannot-f
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { NewsItem, NewsObject, Tag } from "./newsObject";
 const fetch = require('node-fetch').default;
+const dynamoClient = new DynamoDB({});
+//nth refactor as found on https://github.com/aws/aws-sdk-js-v3/blob/main/lib/lib-dynamodb/README.md
+const docClient = DynamoDBDocument.from(dynamoClient);
 
-
-interface NewsItem {
-  id: string,
-  relatedBlog: string,
-  postBody: string,
-  modifiedDate: Date,
-  headlineUrl: string,
-  postDateTime: Date,
-  postSummary: string,
-  headline: string,
-  contentType: string
+interface DynamoDBNewsObject extends NewsItem {
+  tags: {[id: string]: Tag}
 }
 
+function transformEntry(entry: NewsObject): DynamoDBNewsObject {
+  // just moving the tags inside the news item
+  //convert tags to have ids as keys
+  let tagsObject = Object.assign({}, ...entry.tags.map((tag) => ({[tag.id]: tag})));
+  return { 
+    ...entry.item, 
+    tags: tagsObject
+  }
+}
 export async function handler(event: any, context: any) {
 
-  let page = 0;
-  let url = process.env.NEWS_URL + '&page=' + page;
-
   console.log('Scraper started');
-  console.log(url);
-  let more = false;
+  let getResults = true;
+  let page = 0;
+  let searchParameters = [
+    `item.directoryId=whats-new`,
+    `sort_by=item.additionalFields.postDateTime`,
+    `sort_order=desc`,
+    `size=25`,
+    `item.locale=en_US`,
+    `page=${page}`
 
-  const response = await fetch(url);
-  const data = await response.json()
-  data.items.array.forEach((item: any) => {
-    console.log(item);
-  });
-  
+  ]
+
+  while (getResults) {
+    let url = process.env.SEARCH_URL + "?" + searchParameters.join('&');
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+
+
+    data.items.forEach(async (entry: NewsObject) => {
+      const params = {
+        TableName: process.env.TABLE_NAME as string,
+        Item: transformEntry(entry)
+      }
+      //add to dynamo
+
+      try {
+        const response = await docClient.put(params);
+        console.log(response)
+        return response
+      }
+      catch (err) {
+        console.log(err);
+        return err
+      }
+    });
+
+    page++;
+    //TODO: if we need more results then the below statement should not trigger
+    getResults = false;
+  }
+
+
+
   console.log('Scraper finished');
 
 
