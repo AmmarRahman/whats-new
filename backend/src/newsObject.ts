8082b99f5e0a13adf2e06f1c303bb2d90a0638a8
AmmarRahman <ammar.rahman@gmail.com>
2022-03-14 17:15:29 +0000

    added the Object Type

---
+++ backend/src/newsObject.ts
@@ -0,0 +1,44 @@
+
+export interface NewsObject {
+    item: NewsItem;
+    tags: Array<Tag>
+}
+export interface NewsItem {
+
+    id: string,
+    locale: string,
+    name: string,
+    author: string,
+    createdBy: string,
+    lastUpdatedBy: string,
+    numImpressions: number,
+    score: number,
+    dateCreated: Date,
+    dateUpdated: Date,
+    additionalFields: {
+        postBody: string,
+        directoryId: string,
+        relatedBlog: string,
+        modifiedDate?: Date,
+        headlineUrl: string,
+        postDateTime: Date,
+        postSummary: string,
+        headline: string,
+        contentType: string
+
+    }
+
+}
+
+export interface Tag {
+    id: string,
+    locale: string,
+    tagNamespaceId: string,
+    name: string,
+    description: string,
+    createdBy: string,
+    lastUpdatedBy?: string,
+    dateCreated: Date,
+    dateUpdated?: Date
+}
+
