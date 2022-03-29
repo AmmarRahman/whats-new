041c239f05738a7ca11b16dc0fce6ad9ecc03f49
Ammar Rahman <ammar.rahman@gmail.com>
2022-03-29 12:53:49 +0100

    Appsync (#1)
    Minimal appsync api added 
    split the stack

--- backend/bin/whats-new.ts
+++ backend/bin/whats-new.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import 'source-map-support/register';
+import { PresentationStack } from '../lib/presentation-stack';
 import { ScraperStack } from '../lib/scraper-stack';
 
 const app = new cdk.App();
-new ScraperStack(app, 'ScraperStack');
+const scraper = new ScraperStack(app, 'ScraperStack');
+const presentation = new PresentationStack(app, 'PresentationStack', scraper.dbTable);
