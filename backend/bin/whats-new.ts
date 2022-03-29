1c39db9ea91dc103f58bef54cd5171308e05812c
Ammar Rahman <ammar.rahman@gmail.com>
2022-03-29 12:58:11 +0100

    Revert "Appsync (#1)" (#2)
    This reverts commit 041c239f05738a7ca11b16dc0fce6ad9ecc03f49.

--- backend/bin/whats-new.ts
+++ backend/bin/whats-new.ts
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import 'source-map-support/register';
-import { PresentationStack } from '../lib/presentation-stack';
 import { ScraperStack } from '../lib/scraper-stack';
 
 const app = new cdk.App();
-const scraper = new ScraperStack(app, 'ScraperStack');
-const presentation = new PresentationStack(app, 'PresentationStack', scraper.dbTable);
+new ScraperStack(app, 'ScraperStack');
