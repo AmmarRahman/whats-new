eb08025d0635b651677eb685777e4dcfe25b8231
AmmarRahman <ammar.rahman@gmail.com>
2022-03-29 15:59:40 +0100

    updated icons

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable react/react-in-jsx-scope */
 import { Page } from '../../components/Page/Page';
-import { useCounter } from '../../database/counter';
-import { useCounter as useCounterRedux } from '../../store/counter';
+// import { useCounter } from '../../database/counter';
+// import { useCounter as useCounterRedux } from '../../store/counter';
 
 export const Home = (): JSX.Element => {
-  const { counter, handleDecrementClick, handleIncrementClick } = useCounter();
-  const { counter: c, handleDecrementClick: hDC, handleIncrementClick: hIC } = useCounterRedux();
-  const homeText = 'Home';
+  // const { counter, handleDecrementClick, handleIncrementClick } = useCounter();
+  // const { counter: c, handleDecrementClick: hDC, handleIncrementClick: hIC } = useCounterRedux();
+  const homeText = 'New Announcements';
 
   return (
     <Page description={homeText} keywords={homeText} title={homeText}>
-      <p>Dexie Count (Persistent): {counter.count}</p>
+      {/* <p>Dexie Count (Persistent): {counter.count}</p>
       <p>Redux Count: {c.count}</p>
       <button
         onClick={() => {
@@ -29,7 +30,8 @@ export const Home = (): JSX.Element => {
         type="button"
       >
         +
-      </button>
+      </button> */}
+      <div> hello </div>
     </Page>
   );
 };
